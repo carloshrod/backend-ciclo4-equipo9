16ad1a92ed34deb3d03aaae2a1fc4a8ce390c5b3
CarlosHdzR <hernandezrodriguez@uninorte.edu.co>
2021-12-13 00:21:56 -0500

    Enviando datos al Frontend
    * Se crea una BD falsa (db.js) y las respectivas APIs que serán consumidas por el Frontend para mostrar los datos tanto de usuarios como de predios.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-
+const cors = require("cors");
 const app = express();
+const { users, predios } = require('./db');
 
-app.get("/", function(req, res) {
-    res.send("Hola mundo!!!");
+app.use(cors()); //Middleware cors
+app.use(express.json()); //Middleware json()
+
+// app.get("/", function(req, res) {
+//     res.send("Hola mundo!!!");
+// })
+
+// API Listar usuarios:
+app.get("/users", function(req, res) {
+    res.send(users);
+})
+
+// API Listar predios:
+app.get("/predios", function(req, res) {
+    res.send(predios);
 })
 
 app.listen(8080, function() {
